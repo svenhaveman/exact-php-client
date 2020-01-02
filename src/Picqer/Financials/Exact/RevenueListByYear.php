3702d58fae79e78f9fb3bb1fa1651eb6b52d3bb4
svenhaveman <sven@deinternetjongens.nl>
2020-01-02 15:22:50 +0100

    Add RevenueListByYear Entity

---
+++ src/Picqer/Financials/Exact/RevenueListByYear.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Picqer\Financials\Exact;
+
+/**
+ * Class RevenueListByYear.
+ *
+ * @see https://start.exactonline.nl/docs/HlpRestAPIResourcesDetails.aspx?name=ReadFinancialRevenueListByYear
+ *
+ * @property int $Year Year
+ * @property int $Period Period
+ * @property float $Amount amount of revenue
+ */
+class RevenueList extends Model
+{
+    use Query\Findable;
+
+    protected $primaryKey = 'Year';
+
+    protected $fillable = [
+        'Year',
+        'Period',
+        'Amount',
+    ];
+
+    protected $url = 'read/financial/RevenueListByYear';
+}
